126af14d57782515f1f6a40cbcec862fd1b1d223
juancruzmdq <juancruzmdq@gmail.com>
2016-04-04 16:13:03 -0300

    Add Swift Templates

---
+++ xcode/templates/jcgSoftware/Swift Singleton.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,19 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+import Foundation
+
+/** ___FILEBASENAMEASIDENTIFIER___ Class
+
+*/
+class ___FILEBASENAMEASIDENTIFIER___ {
+
+    static let sharedInstance = ___FILEBASENAMEASIDENTIFIER___()
+
+    internal override init(){}
+}
---
+++ xcode/templates/jcgSoftware/SwiftTypes.xctemplate/ClassNSObject/___FILEBASENAME___.swift
@@ -0,0 +1,35 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+// MARK: Imports
+import Foundation
+
+// MARK: Types
+
+
+/**
+ *  ___FILEBASENAMEASIDENTIFIER___ Inherit ___VARIABLE_extendedClass:identifier___
+ */
+class ___FILEBASENAMEASIDENTIFIER___:___VARIABLE_extendedClass:identifier___ {
+    // MARK: Private Properties
+    // MARK: Public Properties
+    // MARK: IBOutlets
+
+    // MARK: Setup & Teardown
+    internal override init(){
+    }
+
+
+    // MARK: Class Methods
+    // MARK: Override Methods
+    // MARK: Private Methods
+    // MARK: public Methods
+    // MARK: Actions
+    // MARK: XXXDataSource / XXXDelegate methods
+
+}
